e5937dd43d7dbcc7435772858638cb27d3b44b07
Abhinav <abhi9651.at@outlook.com>
2025-08-22 13:34:47 +0530

    Fix CORS origin with env variable

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "https://mern-blog-oaax.vercel.app", 
     credentials: true,
   })
 );
